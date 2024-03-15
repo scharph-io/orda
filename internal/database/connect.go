5702dec31f336743db41df15d77c117c098cea41
Philipp Schartlmüller <philipp.schartlmueller@engel.at>
2024-03-15 07:15:09 +0100

    refactor

--- internal/database/connect.go
+++ internal/database/connect.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strconv"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/driver/sqlite"
@@ -19,19 +18,14 @@ var DB *gorm.DB
 // ConnectDB connect to db
 func ConnectDB() {
 	var err error
-	p := config.Config("DB_PORT")
-	port, err := strconv.ParseUint(p, 10, 32)
-	if err != nil {
-		panic(err)
-	}
 
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
 	// "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		config.Config("DB_USER"),
 		config.Config("DB_PASSWORD"),
 		config.Config("DB_HOST"),
-		port,
+		config.Config("DB_PORT"),
 		config.Config("DB_NAME"),
 	)
 	/*
@@ -39,9 +33,6 @@ func ConnectDB() {
 		To handle time.Time correctly, you need to include parseTime as a parameter. (more parameters)
 		To fully support UTF-8 encoding, you need to change charset=utf8 to charset=utf8mb4. See this article for a detailed explanation
 	*/
-
-	fmt.Println(dsn)
-
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -49,7 +40,7 @@ func ConnectDB() {
 
 		DB, err = gorm.Open(sqlite.Open("gorm.db"), &gorm.Config{})
 		if err != nil {
-			log.Fatal("Failed to connect to mysql database. \n", err)
+			log.Fatal("Failed to connect to sqlite database. \n", err)
 			os.Exit(2)
 		}
 	}
