481426a06a7ae0139d18ae780e829d135ad561db
Philipp Schartlmüller <philipp.schartlmueller@engel.at>
2024-03-27 15:45:27 +0100

    update

--- web/client/src/app/orderview/cart/mobile/cart-footer.component.ts
+++ web/client/src/app/orderview/cart/mobile/cart-footer.component.ts
@@ -2,17 +2,23 @@ import { Component, EventEmitter, Output, inject } from '@angular/core';
 import { CartSubtotalComponent } from '../cart-subtotal/cart-subtotal.component';
 import { CartStore } from '../cart.store';
 import { AsyncPipe } from '@angular/common';
-import { MatButtonModule, MatIconButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslocoModule } from '@ngneat/transloco';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'orda-cart-footer',
   standalone: true,
   template: `
+    @if ((totalQty$ | async)) {
+      <button class="item-0" mat-icon-button color="warn" (click)="clearCart()">
+        <mat-icon>delete_forever</mat-icon>
+      </button>
+    }
       <orda-cart-subtotal [subtotal]="(subtotal$ | async) ?? 0"></orda-cart-subtotal>
       <button mat-icon-button color="info" (click)="onOpenCart.emit($event)">
-        <mat-icon>shopping_cart</mat-icon>
+        <mat-icon [matBadge]="(totalQty$ | async)" matBadgeColor="warn"  aria-hidden="false">shopping_cart</mat-icon>
       </button>
 
   `,
@@ -31,7 +37,7 @@ import { TranslocoModule } from '@ngneat/transloco';
       }
     `
   ],
-  imports: [CartSubtotalComponent, AsyncPipe, MatButtonModule, MatIconModule, TranslocoModule]
+  imports: [CartSubtotalComponent, AsyncPipe, MatButtonModule, MatBadgeModule, MatIconModule, TranslocoModule]
 })
 export class CartFooterComponent {
   cartStore = inject(CartStore);
@@ -41,4 +47,13 @@ export class CartFooterComponent {
   get subtotal$() {
     return this.cartStore.subtotal$;
   }
+
+  get totalQty$() {
+    return this.cartStore.totalQty$;
+  }
+
+  clearCart(): void {
+    this.cartStore.clear();
+  }
+
 }
