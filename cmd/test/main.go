7fd7db4d79da16523471127a49fbf4bda4964fb8
Philipp Schartlmüller <philipp.schartlmueller@engel.at>
2024-08-17 13:56:28 +0200

    update

--- cmd/test/main.go
+++ cmd/test/main.go
@@ -26,67 +26,18 @@ func main() {
 
 	ctx := db.Statement.Context
 
-	// Create Group
-	// group, err := groupRepo.Create(ctx, &model.Group{
-	// 	Name: "Group 1",
-	// 	Desc: "Group 1 Desc",
-	// })
-
-	// Get All Groups
-	// groups, err := groupRepo.Read(ctx)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// result, err := productRepo.ImportMany(ctx, &[]model.Product{
-	// 	{
-	// 		Name:  "Cola",
-	// 		Desc:  "0.5",
-	// 		Price: 200,
-	// 	},
-	// 	{
-	// 		Name:  "Cola",
-	// 		Desc:  "0.3",
-	// 		Price: 300,
-	// 	},
-	// 	{
-	// 		Name:  "Cola",
-	// 		Desc:  "0.2",
-	// 		Price: 150,
-	// 	},
-	// }, groups[0].ID)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
-	// fmt.Println("Added Products:", len(result))
-
-	// Get the first 3 products
-	// products, err := productRepo.Read(ctx)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// fmt.Println("Products:", len(products))
-
-	// p := products[:5]
-
-	// _, err := viewRepo.Create(ctx, &model.View{
-	// 	Name: "Kiosk 2",
-	// 	// Products: p,
-	// })
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-
 	views, err := viewRepo.Read(ctx)
 
 	if err != nil {
 		fmt.Println(err)
 	}
-
 	fmt.Printf("Views: %d\n", len(views))
+
+	view := views[0]
+
+	vS := service.NewViewService(viewRepo, productRepo, groupRepo)
+
+	vS.AddProduct(ctx, view.ID, "01915f5d-307a-75d3-b5f1-e62cdb11cadd")
 
 	// res, err := viewProductRepo.ReadByViewId(ctx, views[0].ID)
 	// if err != nil {
@@ -112,8 +63,6 @@ func main() {
 	// 		fmt.Println("--", p.Name, p.Desc, p.Price)
 	// 	}
 	// }
-
-	_ = service.NewViewService(viewRepo, productRepo, groupRepo)
 
 	// v := "01915f7f-36c1-79f7-a050-613526320bf3"
 	// p := "01915f5d-307a-75d3-b5f1-e62cdb11cadd"
