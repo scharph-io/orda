47b890ece2b60f657230157084c1cec71857b649
Philipp Schartlmüller <philipp.schartlmueller@engel.at>
2024-11-30 15:26:22 +0100

    feat: add libs

--- web/client_v2/projects/order/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of order
- */
-
-export * from './lib/order.service';
-export * from './lib/order.component';
--- web/client_v2/projects/statistics/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of statistics
- */
-
-export * from './lib/statistics.service';
-export * from './lib/statistics.component';
