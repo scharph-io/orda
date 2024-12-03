ed399df9cc05a76a575201a10e213567ac4a2ffa
Philipp <philipp.schartlmueller@engel.at>
2024-12-03 16:05:29 +0100

    update

--- web/client_v2/projects/shared/src/public-api.ts
+++ web/client_v2/projects/shared/src/public-api.ts
@@ -2,7 +2,11 @@
  * Public API Surface of shared
  */
 
+import { InjectionToken } from '@angular/core';
+
 export * from './lib/shared.service';
 export * from './lib/shared.component';
 export * from './lib/login/login.component';
 export * from './lib/auth/auth.service';
+
+export const URL_TOKEN = new InjectionToken<string>('URL');
