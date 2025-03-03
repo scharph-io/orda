736984ee6c9dcfb75f67e90192f1f6b7f4464ecf
Philipp Schartlmüller <schartiph@gmail.com>
2025-03-03 22:34:00 +0100

    update account

--- web/clientV2/projects/client/src/app/features/manage/assortment/assortment.component.ts
+++ web/clientV2/projects/client/src/app/features/manage/assortment/assortment.component.ts
@@ -1,27 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
-import { AssortmentGroupService } from '@orda.features/data-access/services/assortment/assortment-group.service';
-import { OrdaLogger } from '@orda.shared/services/logger.service';
 import { RouterModule } from '@angular/router';
-import { AssortmentGroupsComponent } from './groups/groups.component';
+import { AssortmentGroupsComponent } from '@orda.features/manage/assortment/groups/groups.component';
 
 @Component({
 	selector: 'orda-assortment',
 	imports: [MatButtonModule, MatListModule, RouterModule, AssortmentGroupsComponent],
-	template: `
-		<div class="title-toolbar">
-			<h2>Assortment</h2>
-			<orda-assortment-groups />
-		</div>
-	`,
+	template: ` <orda-assortment-groups /> `,
 	styles: ``,
 })
-export class AssortmentComponent {
-	groupService = inject(AssortmentGroupService);
-	logger = inject(OrdaLogger);
-
-	create(): void {
-		console.log('Create new Assortment Group');
-	}
-}
+export class AssortmentComponent {}
--- web/clientV2/projects/client/src/app/features/manage/assortment/products/products.component.ts
+++ web/clientV2/projects/client/src/app/features/manage/assortment/products/products.component.ts
@@ -2,16 +2,16 @@ import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { AssortmentGroupService } from '@orda.features/data-access/services/assortment/assortment-group.service';
-import { OrdaCurrencyPipe } from '@orda.shared/pipes/currency.pipe';
 
 @Component({
 	selector: 'orda-assortment-products',
 	template: `
-		@for (product of products.value(); track product.id) {
-			{{ product.name }} ({{ product.desc }}) {{ product.price | currency }}<br />
-		}
+		products
+		<!--		@for (product of products.value(); track product.id) {-->
+		<!--			{{ product.name }} ({{ product.desc }}) {{ product.price | currency }}<br />-->
+		<!--		}-->
 	`,
-	imports: [OrdaCurrencyPipe],
+	imports: [],
 	styles: ``,
 })
 export class AssortmentProductsComponent {
