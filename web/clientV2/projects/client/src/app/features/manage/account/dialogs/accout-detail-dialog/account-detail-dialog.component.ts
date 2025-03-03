1aea7af7a9a0e7174596c090a65f548900604153
Philipp Schartlmüller <schartiph@gmail.com>
2025-03-03 07:07:42 +0100

    update account

---
+++ web/clientV2/projects/client/src/app/features/manage/account/dialogs/accout-detail-dialog/account-detail-dialog.component.ts
@@ -0,0 +1,46 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButton } from '@angular/material/button';
+import {
+	MAT_DIALOG_DATA,
+	MatDialogActions,
+	MatDialogClose,
+	MatDialogContent,
+	MatDialogRef,
+	MatDialogTitle,
+} from '@angular/material/dialog';
+import { Account } from '@orda.core/models/account';
+
+import { JsonPipe } from '@angular/common';
+
+@Component({
+	selector: 'orda-account-details-dialog',
+	imports: [
+		FormsModule,
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatButton,
+		MatDialogActions,
+		MatDialogTitle,
+		MatDialogContent,
+		MatDialogClose,
+		JsonPipe,
+	],
+	template: ` <h2 mat-dialog-title>Details</h2>
+		<mat-dialog-content>
+			{{ account | json }}
+
+			<button mat-button>Delete</button>
+			<button mat-button>Edit</button>
+		</mat-dialog-content>
+		<mat-dialog-actions>
+			<button mat-button mat-dialog-close>Close</button>
+		</mat-dialog-actions>
+		‚`,
+	styles: ``,
+})
+export class AccountDetailDialogComponent {
+	protected readonly account = inject<Account>(MAT_DIALOG_DATA);
+	protected readonly dialogRef: MatDialogRef<AccountDetailDialogComponent> = inject(MatDialogRef);
+}
---
+++ web/clientV2/projects/client/src/app/features/manage/account/dialogs/group-deposit-dialog/group-deposit.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GroupDepositComponent } from './group-deposit.component';
+
+describe('GroupDepositComponent', () => {
+  let component: GroupDepositComponent;
+  let fixture: ComponentFixture<GroupDepositComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GroupDepositComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(GroupDepositComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
