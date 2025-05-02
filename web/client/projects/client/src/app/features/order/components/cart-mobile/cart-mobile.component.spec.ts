b93c4d20b0f6deb9cf1347127604f399b4fd65c4
Philipp Schartlmüller <schartiph@gmail.com>
2025-05-02 09:57:54 +0200

    feat: mobile cart

---
+++ web/client/projects/client/src/app/features/order/components/cart-mobile/cart-mobile.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CartMobileComponent } from './cart-mobile.component';
+
+describe('CartMobileComponent', () => {
+	let component: CartMobileComponent;
+	let fixture: ComponentFixture<CartMobileComponent>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [CartMobileComponent],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(CartMobileComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
