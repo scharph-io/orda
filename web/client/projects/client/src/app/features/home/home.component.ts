585eb5bbe8710e7a28e4f1d09be7a7ac988a02b8
Philipp Schartlmüller <schartiph@gmail.com>
2025-12-08 22:05:26 +0100

    tailwind

--- web/client/projects/client/src/app/features/home/home.component.ts
+++ web/client/projects/client/src/app/features/home/home.component.ts
@@ -21,7 +21,48 @@ export interface Tile {
 			{{ sessionService.user().username }}
 		</h1>
 
-		<mat-grid-list class="manage-list" [cols]="gridCol.size()" [gutterSize]="'0.5rem'">
+		<div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
+			<div class="md:flex">
+				<div class="md:shrink-0">
+					<img
+						class="h-48 w-full object-cover md:h-full md:w-48"
+						src="/img/building.jpg"
+						alt="Modern building architecture"
+					/>
+				</div>
+				<div class="p-8">
+					<div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
+						Company retreats
+					</div>
+					<a
+						href="#"
+						class="mt-1 block text-lg leading-tight font-medium text-black hover:underline"
+					>
+						Incredible accommodation for your team
+					</a>
+					<p class="mt-2 text-gray-500">
+						Looking to take your team away on a retreat to enjoy awesome food and take in some
+						sunshine? We have a list of places to do just that.
+					</p>
+				</div>
+			</div>
+		</div>
+
+		<div>
+			<div class="bg-sky-50"></div>
+			<div class="bg-sky-100"></div>
+			<div class="bg-sky-200"></div>
+			<div class="bg-sky-300"></div>
+			<div class="bg-sky-400"></div>
+			<div class="bg-sky-500"></div>
+			<div class="bg-sky-600"></div>
+			<div class="bg-sky-700"></div>
+			<div class="bg-sky-800"></div>
+			<div class="bg-sky-900"></div>
+			<div class="bg-sky-950"></div>
+		</div>
+
+		<mat-grid-list class="manage-list " [cols]="gridCol.size()" [gutterSize]="'0.5rem'">
 			@for (tile of primaryTiles(); track tile) {
 				@if (tile.canActivate ? tile.canActivate() : true) {
 					<mat-grid-tile (click)="navigateTo(tile.path)" [colspan]="1" [rowspan]="1">
@@ -50,6 +91,7 @@ export interface Tile {
 	styles: `
 		h1 {
 			margin-top: 5vh;
+			color: var(--color-red-800);
 		}
 
 		mat-icon {
