a765dd96823c4d46e41f96755a88a99a90f23f05
Philipp Schartlmüller <schartiph@gmail.com>
2025-12-10 01:52:40 +0100

    update

--- web/client/projects/client/src/app/features/home/home.component.ts
+++ web/client/projects/client/src/app/features/home/home.component.ts
@@ -16,103 +16,39 @@ export interface Tile {
   selector: 'orda-home',
   imports: [MatGridListModule, MatIcon],
   template: `
-    <h1>
-      Wilkommen,
-      {{ sessionService.user().username }}
-    </h1>
-
-    <div class="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
-      <div class="md:flex">
-        <div class="md:shrink-0">
-          <img
-            class="h-48 w-full object-cover md:h-full md:w-48"
-            src="/img/building.jpg"
-            alt="Modern building architecture"
-          />
+    <div class="min-h-full">
+      <header
+        class="relative after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10"
+      >
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 class="text-center text-4xl font-bold">
+            Wilkommen, {{ sessionService.user().username }}
+          </h1>
         </div>
-        <div class="p-8">
-          <div class="text-sm font-semibold tracking-wide text-indigo-500 uppercase">
-            Company retreats
-          </div>
-          <a
-            href="#"
-            class="mt-1 block text-lg leading-tight font-medium text-black hover:underline"
-          >
-            Incredible accommodation for your team
-          </a>
-          <p class="mt-2 text-gray-500">
-            Looking to take your team away on a retreat to enjoy awesome food and take in some
-            sunshine? We have a list of places to do just that.
-          </p>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <ul class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            @for (tile of primaryTiles(); track tile) {
+              <li (click)="navigateTo(tile.path)" (keydown)="navigateTo(tile.path)" tabindex="0">
+                <a
+                  class="flex flex-col items-center justify-center rounded-xl border border-gray-200 px-4 py-6 shadow-sm hover:shadow-md hover:bg-gray-50 transition"
+                >
+                  <span class="text-2xl mb-2"
+                    ><mat-icon class="icon-large">
+                      {{ tile.icon }}
+                    </mat-icon></span
+                  >
+                  <span class="text-lg font-bold">{{ tile.title }}</span>
+                </a>
+              </li>
+            }
+          </ul>
         </div>
-      </div>
+      </main>
     </div>
-
-    <div>
-      <div class="bg-sky-50"></div>
-      <div class="bg-sky-100"></div>
-      <div class="bg-sky-200"></div>
-      <div class="bg-sky-300"></div>
-      <div class="bg-sky-400"></div>
-      <div class="bg-sky-500"></div>
-      <div class="bg-sky-600"></div>
-      <div class="bg-sky-700"></div>
-      <div class="bg-sky-800"></div>
-      <div class="bg-sky-900"></div>
-      <div class="bg-sky-950"></div>
-    </div>
-
-    <mat-grid-list class="manage-list " [cols]="gridCol.size()" [gutterSize]="'0.5rem'">
-      @for (tile of primaryTiles(); track tile) {
-        @if (tile.canActivate ? tile.canActivate() : true) {
-          <mat-grid-tile (click)="navigateTo(tile.path)" [colspan]="1" [rowspan]="1">
-            <div class="tile-content">
-              <mat-icon aria-hidden="false">
-                {{ tile.icon }}
-              </mat-icon>
-              <span class="tile-text">{{ tile.title }}</span>
-            </div>
-          </mat-grid-tile>
-        }
-      }
-    </mat-grid-list>
-
-    <!-- <h2>TODOs for v0.2.1</h2>
-		<ul>
-			<li>Colors</li>
-			<li>Order UI settings set grid cols to auto, 2-5</li>
-			<li>Manual deposit</li>
-		</ul> -->
-
-    <!--		https://lucide.dev/guide/packages/lucide-angular-->
-    <!--		https://sebastianviereck.de/httponly-und-secure-cookies-in-angular/-->
-    <!-- https://taiga-ui.dev/utils/tokens -->
   `,
-  styles: `
-    h1 {
-      margin-top: 5vh;
-      color: var(--color-red-800);
-    }
-
-    mat-icon {
-      height: 3rem;
-      width: 3rem;
-      font-size: 3rem;
-    }
-
-    .tile-content {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      height: 100%;
-    }
-
-    .tile-text {
-      margin-top: 0.25rem;
-      font-size: 1.5rem;
-    }
-  `,
+  styles: ``,
 })
 export class HomeComponent {
   sessionService = inject(SessionService);
--- web/client/projects/client/src/app/features/manage/manage.component.ts
+++ web/client/projects/client/src/app/features/manage/manage.component.ts
@@ -11,51 +11,58 @@ import { Tile } from '@orda.features/home/home.component';
   selector: 'orda-manage',
   imports: [RouterModule, MatButtonModule, MatGridListModule, MatRipple, MatIcon],
   template: `
-    <h1>Verwalten</h1>
-    <mat-grid-list class="manage-list" [cols]="gridColumns()" [gutterSize]="'0.5rem'">
-      @for (tile of primaryTiles(); track tile) {
-        <mat-grid-tile mat-ripple (click)="navigateTo(tile.path)" [colspan]="1" [rowspan]="1"
-          ><div class="tile-content">
-            <mat-icon aria-hidden="false">
-              {{ tile.icon }}
-            </mat-icon>
-            <span class="tile-text">{{ tile.title }}</span>
+    <div class="min-h-full">
+      <header
+        class="relative after:pointer-events-none after:absolute after:inset-x-0 after:inset-y-0 after:border-y after:border-white/10"
+      >
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <div class="flex">
+            <button mat-icon-button class="mr-4" (click)="navigateBack()">
+              <mat-icon class="tile">arrow_back</mat-icon>
+            </button>
+            <h1 class="text-center text-4xl font-bold">Verwalten</h1>
           </div>
-        </mat-grid-tile>
-      }
-    </mat-grid-list>
+        </div>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <ul role="list" class="divide-y divide-white/5">
+            @for (tile of primaryTiles(); track tile) {
+              <li
+                class="flex justify-center gap-x-4 py-3"
+                tabindex="0"
+                (click)="navigateTo(tile.path)"
+                (keydown)="navigateTo(tile.path)"
+              >
+                <div
+                  class="flex min-w-0 gap-x-4 items-center place-content-stretch"
+                  (click)="navigateTo(tile.path)"
+                  (keydown)="navigateTo(tile.path)"
+                  tabindex="0"
+                >
+                  <mat-icon class="icon-large">
+                    {{ tile.icon }}
+                  </mat-icon>
+                  <div class="min-w-0 flex-auto">
+                    <p class="text-xl font-semibold">{{ tile.title }}</p>
+                  </div>
+                  <div class="w-30"></div>
+                  <mat-icon class="size-14 grow"> chevron_right </mat-icon>
+                </div>
+              </li>
+            }
+          </ul>
+        </div>
+      </main>
+    </div>
   `,
-  styles: `
-    h1 {
-      margin-top: 5vh;
-    }
-
-    mat-icon {
-      height: 3rem;
-      width: 3rem;
-      font-size: 3rem;
-    }
-
-    .tile-content {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      height: 100%;
-    }
-
-    .tile-text {
-      margin-top: 0.25rem;
-      font-size: 1.25rem;
-    }
-  `,
+  styles: ``,
 })
 export class ManageComponent {
   private readonly router = inject(Router);
   gridColumns = inject(GridColSizeService).size;
 
   primaryTiles = signal<Tile[]>([
-    { title: 'Zurück', path: '/home', icon: 'arrow_back' },
     { title: 'Bestellseiten', path: '/manage/views', icon: 'view_list' },
     {
       title: 'Sortiment',
@@ -70,5 +77,9 @@ export class ManageComponent {
 
   navigateTo(path: string) {
     this.router.navigate([path]).catch(console.error);
+  }
+
+  navigateBack() {
+    this.router.navigate(['/home']).catch(console.error);
   }
 }
